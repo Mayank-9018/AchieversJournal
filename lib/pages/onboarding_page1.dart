c9ff7efe44eea56184fcacd4df05c672ce782f29
Mayank Gupta <ubuntu568@gmail.com>
2021-12-16 21:06:21 +0530

    Update Application name and fix OnboardingPage1 layout

--- lib/pages/onboarding_page1.dart
+++ lib/pages/onboarding_page1.dart
@@ -8,27 +8,32 @@ class OnboardingPage1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 80.0, horizontal: 40.0),
+      padding:
+          const EdgeInsets.only(top: 60.0, left: 40.0, right: 40.0, bottom: 30),
       child: Column(
         children: [
-          Text(
-            'Effectively track your goals',
-            style: Theme.of(context).textTheme.headline3,
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          const ProgressBar(Icons.chrome_reader_mode_outlined, 'Read', 60),
-          const SizedBox(
-            height: 10,
-          ),
-          const ProgressBar(Icons.self_improvement, 'Meditate', 100),
-          const SizedBox(
-            height: 10,
-          ),
-          const ProgressBar(Icons.translate, 'Practice German', 15),
-          const SizedBox(
-            height: 100,
+          Expanded(
+            child: ListView(
+              children: [
+                Text(
+                  'Effectively track your goals',
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const ProgressBar(
+                    Icons.chrome_reader_mode_outlined, 'Read', 60),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ProgressBar(Icons.self_improvement, 'Meditate', 100),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ProgressBar(Icons.translate, 'Practice German', 15),
+              ],
+            ),
           ),
           const PageIndicator(currentPage: 0, totalPages: 3)
         ],
