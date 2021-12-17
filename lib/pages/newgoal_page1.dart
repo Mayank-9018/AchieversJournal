b323ab028c7f73f499fca84d937f75831cff7d50
Mayank Gupta <ubuntu568@gmail.com>
2021-12-17 14:46:46 +0530

    New goal Screen and Page 1

---
+++ lib/pages/newgoal_page1.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class NewGoalPage1 extends StatelessWidget {
+  const NewGoalPage1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 50),
+      children: [
+        Text(
+          "What's your goal?",
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        const TextField(
+          decoration: InputDecoration(
+            hintText: 'Title',
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        const TextField(
+          maxLines: 2,
+          decoration: InputDecoration(
+            hintText: 'Description(optional)',
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/new_goal.dart
@@ -0,0 +1,65 @@
+import 'package:achievers_journal/pages/newgoal_page1.dart';
+import 'package:flutter/material.dart';
+
+class NewGoalScreen extends StatefulWidget {
+  const NewGoalScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NewGoalScreen> createState() => _NewGoalScreenState();
+}
+
+class _NewGoalScreenState extends State<NewGoalScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Set a new goal',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            Expanded(
+              child: PageView(
+                children: const [
+                  NewGoalPage1(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 10.0,
+        ),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0, -2),
+                blurRadius: 10.0,
+                spreadRadius: 1),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text('Continue'),
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
