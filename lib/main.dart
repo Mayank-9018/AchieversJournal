29024b385f0782f31c155c5f3679f429cb37d5cb
Mayank Gupta <ubuntu568@gmail.com>
2021-12-20 19:10:22 +0530

    Add firebase_core and initialize firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:achievers_journal/screens/onboarding.dart';
 import 'package:achievers_journal/themes/dark_theme.dart';
 import 'package:achievers_journal/themes/light_theme.dart';
-import 'package:flutter/material.dart';
 
-void main() => runApp(const AJApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const AJApp());
+}
 
 class AJApp extends StatelessWidget {
   const AJApp({Key? key}) : super(key: key);
