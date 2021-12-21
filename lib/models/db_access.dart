cdab6e15cddf361a6998d44981c04110a5956fa0
Mayank Gupta <ubuntu568@gmail.com>
2021-12-21 18:24:25 +0530

    Database access model

---
+++ lib/models/db_access.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Database {
+  late Future<bool> isLoggedIn;
+
+  Database() {
+    isLoggedIn = getLoginStatus();
+  }
+
+  /// Reads the `SharedPreferences` to check if the user is logged in/syncing data;
+  /// Returns type `bool`
+  Future<bool> getLoginStatus() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    return _prefs.getBool('isLoggedIn') ?? false;
+  }
+
+  /// Returns a `Stream` of type `DatabaseEvent` from the firebase
+  /// realtime database; Used when user opts in to sync data;
+  Stream<DatabaseEvent> rdbData() {
+    FirebaseDatabase firebaseInstance = FirebaseDatabase.instance;
+    return firebaseInstance.ref('/userId').onValue;
+  }
+
+  /// Returns a `Future` of Map already jsonDecoded
+  /// from the the local data
+  Future<Map<String, dynamic>> localData() async {
+    final path = await _localPath;
+    File _localFile = File('$path/data.json');
+    if (await _localFile.exists()) {
+      return jsonDecode(_localFile.readAsStringSync());
+    } else {
+      _localFile.create();
+      return {};
+    }
+  }
+
+  /// Gets the localPath; Used to get the directory
+  /// where to read and store the data from
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+}
