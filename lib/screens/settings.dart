c69004ac80c7d8c8a8bc7d93a6b9ec5ad99e4271
Mayank Gupta <ubuntu568@gmail.com>
2021-12-23 19:59:37 +0530

    Settings Screen

---
+++ lib/screens/settings.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          const SliverAppBar(
+            pinned: true,
+            expandedHeight: 150.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'Settings',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate.fixed(
+              [
+                ListTile(
+                  title: const Text(
+                    'Sign in with Google',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  subtitle: const Text(
+                      'Sign in with Google to sync data between devices.'),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 40.0),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text(
+                    'Theme mode',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  subtitle: const Text('Current: System'),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 40.0),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
