2d28f4b0ee7fc3933e216cf0db5bf42de6626360
Mayank Gupta <ubuntu568@gmail.com>
2021-12-27 19:44:52 +0530

    IconPicker on NewGoalPage1

--- lib/pages/newgoal_page1.dart
+++ lib/pages/newgoal_page1.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconpicker/flutter_iconpicker.dart';
 
-class NewGoalPage1 extends StatelessWidget {
+class NewGoalPage1 extends StatefulWidget {
   const NewGoalPage1({Key? key}) : super(key: key);
+
+  @override
+  State<NewGoalPage1> createState() => _NewGoalPage1State();
+}
+
+class _NewGoalPage1State extends State<NewGoalPage1> {
+  IconData? _icon;
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +23,35 @@ class NewGoalPage1 extends StatelessWidget {
         const SizedBox(
           height: 50,
         ),
-        const TextField(
-          decoration: InputDecoration(
-            hintText: 'Title',
-          ),
+        Row(
+          children: [
+            const Flexible(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Title',
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            InkWell(
+              borderRadius: BorderRadius.circular(14.0),
+              onTap: () => _pickIcon(context),
+              child: Container(
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(14.0),
+                ),
+                child: Icon(
+                  _icon ?? Icons.do_not_disturb,
+                  size: 28.0,
+                  color: _icon == null ? Colors.grey : null,
+                ),
+              ),
+            )
+          ],
         ),
         const SizedBox(
           height: 15,
@@ -31,5 +64,12 @@ class NewGoalPage1 extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  void _pickIcon(BuildContext context) async {
+    IconData? icon = await FlutterIconPicker.showIconPicker(context,
+        iconPackModes: [IconPack.material]);
+    _icon = icon;
+    setState(() {});
   }
 }
