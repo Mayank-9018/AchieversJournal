b332ed2961ae573a7b37e9287cc339135466d6ae
Mayank Gupta <ubuntu568@gmail.com>
2022-01-12 17:38:30 +0530

    Update OnboardingPage2

--- lib/pages/onboarding_page2.dart
+++ lib/pages/onboarding_page2.dart
@@ -1,4 +1,6 @@
 import 'package:achievers_journal/components/page_indicator.dart';
+import 'package:achievers_journal/components/weekly_analysis.dart';
+import 'package:achievers_journal/screens/analytics.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingPage2 extends StatelessWidget {
@@ -8,55 +10,59 @@ class OnboardingPage2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding:
-          const EdgeInsets.only(top: 60.0, left: 40.0, right: 40.0, bottom: 30),
+          const EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0, bottom: 30),
       child: Column(
         children: [
           Expanded(
-            child: ListView(
+            child: Column(
               children: [
+                const SizedBox(height: 10),
                 Text(
-                  'Analytics coming soon!',
+                  'In-depth analytics',
                   style: Theme.of(context).textTheme.headline3,
                 ),
-                const SizedBox(
-                  height: 75,
-                ),
-                Column(
+                const SizedBox(height: 40),
+                const WeeklyAnalysis([0.2, 0.6, 0.4, 0.5, 0.7, 0.6, 1.0]),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Icon(
-                          Icons.trending_up,
-                          size: 100,
-                        ),
-                        Icon(
-                          Icons.insights,
-                          size: 100,
-                        ),
-                      ],
+                    StatCard(
+                      Column(
+                        children: [
+                          Text(
+                            'Average Goals\nCompletion Rate',
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            '56%',
+                            style: Theme.of(context).textTheme.headline2,
+                          ),
+                        ],
+                      ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.bar_chart,
-                          size: 150,
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Icon(
-                          Icons.trending_down,
-                          size: 100,
-                        ),
-                        Icon(
-                          Icons.data_usage,
-                          size: 75,
-                        ),
-                      ],
+                    StatCard(
+                      Column(
+                        children: [
+                          const SizedBox(height: 10),
+                          Text(
+                            'Trend',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(fontSize: 26),
+                          ),
+                          const SizedBox(height: 15),
+                          Text(
+                            '+12%',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline2!
+                                .copyWith(color: Colors.green),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
